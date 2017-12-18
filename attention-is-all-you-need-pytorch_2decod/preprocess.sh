a712340c3d59fdc6e0b61ef42378277d79b6886d
Gonaco <danielmoremanza@gmail.com>
2017-12-18 20:15:01 +0100

    MArc SAvED me

--- attention-is-all-you-need-pytorch_2decod/preprocess.sh
+++ attention-is-all-you-need-pytorch_2decod/preprocess.sh
@@ -1,9 +1,10 @@
 
-python ../analysis/dataPreprocess.py ../data/kaggle_norm_competition/en_train.csv 3
-python ../analysis/dataPreprocess.py ../data/kaggle_norm_competition/en_train.csv 4
-python ../analysis/dataPreprocess.py ../data/kaggle_norm_competition/en_test.csv 2
+# python ../analysis/dataPreprocess.py ../data/kaggle_norm_competition/en_train.csv 2
+# python ../analysis/dataPreprocess.py ../data/kaggle_norm_competition/en_train.csv 3
+# python ../analysis/dataPreprocess.py ../data/kaggle_norm_competition/en_train.csv 4
+# python ../analysis/dataPreprocess.py ../data/kaggle_norm_competition/en_test.csv 2
 
 # for l in en de; do for f in data/kaggle_norm_competition/*.$l; do if [[ "$f" != *"test"* ]]; then sed -i "$ d" $f; fi;  done; done
 # for l in en de; do for f in data/kaggle_norm_competition/*.$l; do perl tokenizer.perl -a -no-escape -l $l -q  < $f > $f.atok; done; done
 
-python preprocess.py -train_src ../data/kaggle_norm_competition/en_train_SRC.txt -train_tgt ../data/kaggle_norm_competition/en_train_DST.txt -valid_src ../data/kaggle_norm_competition/en_val_SRC.txt -valid_tgt ../data/kaggle_norm_competition/en_val_DST.txt -save_data ../data/kaggle_norm_competition/train_kaggle2transformer.atok.low.pt
+python preprocess.py -train_src ../data/kaggle_norm_competition/en_train_SRC.txt -train_tgt ../data/kaggle_norm_competition/en_train_DST.txt -classes_tgt ../data/kaggle_norm_competition/en_train_CLASS.txt -valid_src ../data/kaggle_norm_competition/en_val_SRC.txt -valid_tgt ../data/kaggle_norm_competition/en_val_DST.txt -save_data ../data/kaggle_norm_competition/train_kaggle2transformer.atok.low.pt
